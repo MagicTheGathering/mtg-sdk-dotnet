1f2033d6985e75134b4fafaeef5a1b51b08675f2
jason <jason.regnier4@gmail.com>
2016-11-01 23:14:45 -0400

    Created a ratelimit object used to keep track of the calls to the API in order to control and spread out the calls based on the Rate Limit of the API.

---
+++ MtgApiManager.Lib.Test/Core/RateLimitTest.cs
@@ -0,0 +1,81 @@
+﻿// <copyright file="RateLimitTest.cs">
+//     Copyright (c) 2014. All rights reserved.
+// </copyright>
+// <author>Jason Regnier</author>
+namespace MtgApiManager.Lib.Test.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MtgApiManager.Lib.Core;
+
+    /// <summary>
+    /// Tests the <see cref="RateLimit"/> class.
+    /// </summary>
+    [TestClass]
+    public class RateLimitTest
+    {
+        /// <summary>
+        /// Tests the <see cref="RateLimit.AddApiCall"/> method.
+        /// </summary>
+        [TestMethod]
+        public void AddApiCallTest()
+        {
+            var limit = new RateLimit();
+            var privateObject = new PrivateObject(limit);
+
+            limit.AddApiCall();
+            Task.Delay(500);
+            limit.AddApiCall();
+            Task.Delay(500);
+            limit.AddApiCall();
+
+            Assert.AreEqual(3, (privateObject.GetFieldOrProperty("_webServiceCalls") as HashSet<DateTime>).Count);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="RateLimit.RateLimit"/> constructor.
+        /// </summary>
+        [TestMethod]
+        public void ContructerTest()
+        {
+            var limit = new RateLimit();
+            var privateObject = new PrivateObject(limit);
+            Assert.IsNotNull(privateObject.GetFieldOrProperty("_webServiceCalls"));
+        }
+
+        /// <summary>
+        /// Tests the <see cref="RateLimit.GetDelay(int)"/> method.
+        /// </summary>
+        [TestMethod]
+        public void GetDelayTest()
+        {
+            var limit = new RateLimit();
+
+            // Test passing 0 for the request per hour.
+            Assert.AreEqual(0, limit.GetDelay(0));
+
+            // No web calls yet.
+            Assert.AreEqual(0, limit.GetDelay(2000));
+
+            // Test being under the limit.
+            limit.AddApiCall();
+            Thread.Sleep(500);
+            limit.AddApiCall();
+            Assert.AreEqual(0, limit.GetDelay(2000));
+
+            // Test being over the limit.
+            Thread.Sleep(500);
+            limit.AddApiCall();
+            Thread.Sleep(500);
+            limit.AddApiCall();
+            Thread.Sleep(500);
+            limit.AddApiCall();
+            Thread.Sleep(500);
+            limit.AddApiCall();
+            Assert.AreNotEqual(0, limit.GetDelay(2000));
+        }
+    }
+}
---
+++ MtgApiManager.Lib/Core/RateLimit.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="RateLimit.cs">
+//     Copyright (c) 2014. All rights reserved.
+// </copyright>
+// <author>Jason Regnier</author>
+namespace MtgApiManager.Lib.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Object representing a rate limit which helps spread out the calls to API.
+    /// </summary>
+    internal class RateLimit
+    {
+        /// <summary>
+        /// The calls that have been made to the web service.
+        /// </summary>
+        private List<DateTime> _webServiceCalls;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RateLimit"/> class.
+        /// </summary>
+        public RateLimit()
+        {
+            this._webServiceCalls = new List<DateTime>();
+        }
+
+        /// <summary>
+        /// Add a new call to the managed collection.
+        /// </summary>
+        public void AddApiCall()
+        {
+            this._webServiceCalls.Add(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Returns the delay needed to make the next call. The per hour rate will get converted to a per 10 second rate in
+        /// order to spread out the calls over the hour.
+        /// </summary>
+        /// <param name="requestsPerHour">The number of calls permitted per hour.</param>
+        /// <returns>the delay in milliseconds.</returns>
+        public int GetDelay(int requestsPerHour)
+        {
+            int delay = 0;
+
+            if (requestsPerHour == 0)
+            {
+                return 0;
+            }
+
+            // Figure out the requests per 10 seconds in order to spread out the calls.
+            var requestsPerTenSeconds = (int)Math.Floor((float)requestsPerHour / (3600f / 10f));
+
+            if (this._webServiceCalls.Any())
+            {
+                // Remove any extra traces that are not in the last seconds plus a 5 second buffer.
+                this._webServiceCalls.RemoveAll(x => x < DateTime.Now.AddSeconds(-10));
+
+                // Get the request that are within the rate passed in.
+                var lastTenSeconds = this._webServiceCalls
+                                        .Where(x => x > DateTime.Now.AddSeconds(-10))
+                                        .OrderBy(x => x)
+                                        .ToList();
+
+                // If the limit has been reached then calculate the needed delay.
+                if (lastTenSeconds.Count >= requestsPerTenSeconds)
+                {
+                    TimeSpan diff = lastTenSeconds.First() - DateTime.Now.AddSeconds(-10);
+                    delay = (int)Math.Abs(diff.TotalMilliseconds);
+                }
+            }
+
+            return delay;
+        }
+    }
+}
