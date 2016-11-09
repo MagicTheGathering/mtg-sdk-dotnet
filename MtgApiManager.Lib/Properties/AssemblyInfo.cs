82b12db0cc666fcdf205c737c9dadde13238ac70
jason <jason.regnier4@gmail.com>
2016-11-08 23:19:10 -0500

    Updated assembly info. Updated nuget package stuff

--- MtgApiManager.Lib/Properties/AssemblyInfo.cs
+++ MtgApiManager.Lib/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MtgApiManager.Lib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is the Magic: The Gathering SDK C# .NET implementation. It is a wrapper around the MTG API of magicthegathering.io.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jason Regnier")]
 [assembly: AssemblyProduct("MtgApiManager.Lib")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
