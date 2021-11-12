c91fb70a05b7f0a1cfeef1b7c8bcfba38b8f2f17
Your Name <noahluvsmilkduds@gmail.com>
2021-11-11 18:41:43 -0600

    add id and multiverseid query parameters

--- src/MtgApiManager.Lib.Test/Service/CardServiceTests.cs
+++ src/MtgApiManager.Lib.Test/Service/CardServiceTests.cs
@@ -119,16 +119,16 @@ namespace MtgApiManager.Lib.Test.Service
         [Fact]
         public async Task AllAsync_NoMock_EqualPageSize()
         {
-            const int pageSize = 50;
+            const int PAGE_SIZE = 50;
             var serviceProvider = new MtgServiceProvider();
             var service = serviceProvider.GetCardService();
 
             var result = await service
                 .Where(x => x.Page, 1)
-                .Where(x => x.PageSize, pageSize)
+                .Where(x => x.PageSize, PAGE_SIZE)
                 .AllAsync();
 
-            Assert.Equal(pageSize, result.PagingInfo.PageSize);
+            Assert.Equal(PAGE_SIZE, result.PagingInfo.PageSize);
         }
 
         [Fact]
@@ -535,6 +535,87 @@ namespace MtgApiManager.Lib.Test.Service
                 .WithQueryParams("name", "language");
         }
 
+
+        [Fact]
+        public async Task Where_AddsIdParameter_Success()
+        {
+            const string MULTIPLE_IDS = 
+                "896a92b1-ed29-5daf-bf89-2502224f8f11|516dd7a4-fad8-5eed-bcdb-c05088762303";
+            
+            const string ID_PARAM = "id";
+
+            _mockRateLimit.Setup(x => x.IsTurnedOn).Returns(false);
+
+            _mockHeaderManager.Setup(x => x.Update(It.IsAny<IReadOnlyNameValueList<string>>()));
+            _mockHeaderManager.Setup(x => x.Get<int>(ResponseHeader.TotalCount)).Returns(2000);
+            _mockHeaderManager.Setup(x => x.Get<int>(ResponseHeader.PageSize)).Returns(1000);
+
+            var rootCardList = new RootCardListDto()
+            {
+                Cards = new List<CardDto> { new CardDto() },
+            };
+
+            using var httpTest = new HttpTest();
+            httpTest.RespondWithJson(rootCardList);
+
+            _mockModelMapper.Setup(x => x.MapCard(It.IsAny<CardDto>())).Returns(new Card());
+
+            var service = new CardService(
+                _mockHeaderManager.Object,
+                _mockModelMapper.Object,
+                ApiVersion.V1,
+                _mockRateLimit.Object);
+
+            // act
+            await service
+                .Where(x => x.Id, MULTIPLE_IDS)
+                .AllAsync();
+
+            // assert
+            httpTest
+                .ShouldHaveCalled("https://api.magicthegathering.io/v1/cards*")
+                .WithQueryParams(ID_PARAM);
+        }
+
+        [Fact]
+        public async Task Where_AddMultiverseIdParameter_Success()
+        {
+            const string MULTIPLE_MULTIS = "3|4";
+            const string MULTI_PARAM = "multiverseid";
+
+            _mockRateLimit.Setup(x => x.IsTurnedOn).Returns(false);
+
+            _mockHeaderManager.Setup(x => x.Update(It.IsAny<IReadOnlyNameValueList<string>>()));
+            _mockHeaderManager.Setup(x => x.Get<int>(ResponseHeader.TotalCount)).Returns(2000);
+            _mockHeaderManager.Setup(x => x.Get<int>(ResponseHeader.PageSize)).Returns(1000);
+
+            var rootCardList = new RootCardListDto()
+            {
+                Cards = new List<CardDto> { new CardDto() },
+            };
+
+            using var httpTest = new HttpTest();
+            httpTest.RespondWithJson(rootCardList);
+
+            _mockModelMapper.Setup(x => x.MapCard(It.IsAny<CardDto>())).Returns(new Card());
+
+            var service = new CardService(
+                _mockHeaderManager.Object,
+                _mockModelMapper.Object,
+                ApiVersion.V1,
+                _mockRateLimit.Object);
+
+            // act
+            await service
+                .Where(x => x.MultiverseId, MULTIPLE_MULTIS)
+                .AllAsync();
+
+            // assert
+            httpTest
+                .ShouldHaveCalled("https://api.magicthegathering.io/v1/cards*")
+                .WithQueryParams(MULTI_PARAM);
+        }
+
         [Fact]
         public void Where_DefaultValue_Throws()
         {
@@ -563,6 +644,22 @@ namespace MtgApiManager.Lib.Test.Service
             // act
             // assert
             Assert.Throws<ArgumentNullException>(() => service.Where(_ => null, NAME));
+        }
+
+        [Fact]
+        public async Task Where_AddMultiverseIdNoMock_Success()
+        {
+            const string MULTIPLE_MULTIS = "3|4|5|6";
+
+            var serviceProvider = new MtgServiceProvider();
+            var service = serviceProvider.GetCardService();
+
+            var result = await service
+                .Where(x => x.MultiverseId, MULTIPLE_MULTIS)
+                .AllAsync();
+
+            Assert.True(result.IsSuccess);
+            Assert.Equal(4, result.Value.Count);
         }
     }
 }
